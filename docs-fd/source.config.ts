c5dad9c333e92ec5da69cc21d810c2c31cb4ada6
Chafik H'nini <chafik.hnini@gmail.com>
2025-03-04 12:21:41 +0100

    feat: setup math

--- docs-fd/source.config.ts
+++ docs-fd/source.config.ts
@@ -1,5 +1,7 @@
 import { remarkMermaid } from '@theguild/remark-mermaid';
 import { defineConfig, defineDocs } from 'fumadocs-mdx/config';
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
 
 export const docs = defineDocs({
     dir: 'content/docs',
@@ -7,6 +9,8 @@ export const docs = defineDocs({
 
 export default defineConfig({
     mdxOptions: {
-        remarkPlugins: [remarkMermaid],
+        remarkPlugins: [remarkMermaid, remarkMath],
+        // Place it at first so that it won't be changed by syntax highlighter
+        rehypePlugins: (v) => [rehypeKatex, ...v],
     },
 });
--- docs-fd/src/app/docs/layout.tsx
+++ docs-fd/src/app/docs/layout.tsx
@@ -3,6 +3,8 @@ import { source } from '@/lib/source';
 import { DocsLayout } from 'fumadocs-ui/layouts/docs';
 import type { ReactNode } from 'react';
 
+import 'katex/dist/katex.css';
+
 export default function Layout({ children }: { children: ReactNode }) {
     return (
         <DocsLayout
