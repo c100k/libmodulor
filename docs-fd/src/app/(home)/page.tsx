52fdb18ea5e3337c3fec9415183a4ceaa787ecd2
Chafik H'nini <chafik.hnini@gmail.com>
2025-03-04 11:10:28 +0100

    feat: customize

--- docs-fd/src/app/(home)/page.tsx
+++ docs-fd/src/app/(home)/page.tsx
@@ -2,17 +2,24 @@ import Link from 'next/link';
 
 export default function HomePage() {
     return (
-        <main className="flex flex-1 flex-col justify-center text-center">
-            <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
+        <main className="flex flex-1 flex-col justify-center text-center gap-3">
+            <div>
+                <h1 className="mb-4 text-2xl font-bold">libmodulor</h1>
+                <h2>
+                    An opinionated TypeScript library to create business
+                    oriented applications
+                </h2>
+            </div>
+
             <p className="text-fd-muted-foreground">
-                You can open{' '}
+                Read{' '}
                 <Link
                     href="/docs"
                     className="text-fd-foreground font-semibold underline"
                 >
-                    /docs
-                </Link>{' '}
-                and see the documentation.
+                    the docs
+                </Link>
+                .
             </p>
         </main>
     );
