0be116a080c6c45e8b63e85ea13cdf027d68d130
Chafik H'nini <chafik.hnini@gmail.com>
2025-03-06 16:22:17 +0100

    docs: improve home + add banner

--- docs-fd/src/app/(home)/page.tsx
+++ docs-fd/src/app/(home)/page.tsx
@@ -2,24 +2,19 @@ import Link from 'next/link';
 
 export default function HomePage() {
     return (
-        <main className="flex flex-1 flex-col justify-center text-center gap-3">
+        <main className="flex flex-1 flex-col justify-center text-center gap-6">
             <div>
-                <h1 className="mb-4 text-2xl font-bold">libmodulor</h1>
-                <h2>
+                <h1 className="mb-4 text-4xl font-bold">libmodulor</h1>
+                <h2 className="text-2xl">
                     An opinionated TypeScript library to create business
                     oriented applications
                 </h2>
             </div>
 
             <p className="text-fd-muted-foreground">
-                Read{' '}
-                <Link
-                    href="/docs"
-                    className="text-fd-foreground font-semibold underline"
-                >
-                    the docs
+                <Link href="/docs" className="text-2xl font-semibold underline">
+                    read the docs
                 </Link>
-                .
             </p>
         </main>
     );
