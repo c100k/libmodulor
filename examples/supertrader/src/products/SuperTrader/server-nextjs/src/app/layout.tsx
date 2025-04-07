50f045f6950056f4b7c960167b7424c14113b145
Chafik H'nini <chafik.hnini@gmail.com>
2025-04-07 22:29:22 +0200

    fix: init in execReq

--- examples/supertrader/src/products/SuperTrader/server-nextjs/src/app/layout.tsx
+++ examples/supertrader/src/products/SuperTrader/server-nextjs/src/app/layout.tsx
@@ -1,36 +1,14 @@
-import {
-    ExternalResourceInstaller,
-    type I18nManager,
-    type Logger,
-    UCDataStoreExternalResourceManager,
-} from 'libmodulor';
 import React, { type PropsWithChildren, type ReactElement } from 'react';
 
 import ClientLayout from '../components/ClientLayout.js';
-import container from '../container-server.js';
 
 import './styles.css';
-
-const i18nManager = container.get<I18nManager>('I18nManager');
-const logger = container.get<Logger>('Logger');
-
-logger.info('Initializing server');
-logger.info('Initializing i18n');
-await i18nManager.init();
-logger.info('Done initializing i18n');
-await container.get(ExternalResourceInstaller).exec({
-    autoGenerate: true,
-    force: false,
-    manager: container.get(UCDataStoreExternalResourceManager),
-    onFeedback: async (feedback) => logger.info(feedback.message),
-});
-logger.info('Done initializing server');
 
 export default function RootLayout({
     children,
 }: PropsWithChildren): ReactElement {
     return (
-        <html lang={i18nManager.l()}>
+        <html lang="en">
             <body>
                 <ClientLayout>{children}</ClientLayout>
             </body>
