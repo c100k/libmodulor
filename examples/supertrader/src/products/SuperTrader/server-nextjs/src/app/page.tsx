c3e6ad0ae256d8188162b66b491413a69222e061
Chafik H'nini <chafik.hnini@gmail.com>
2025-04-07 22:49:42 +0200

    refactor: centralize shared react components

--- examples/supertrader/src/products/SuperTrader/server-nextjs/src/app/page.tsx
+++ examples/supertrader/src/products/SuperTrader/server-nextjs/src/app/page.tsx
@@ -1,6 +1,6 @@
 import React, { type ReactElement } from 'react';
 
-import App from '../components/App.js';
+import App from '../../../_lib/react/web/App.js';
 
 export default function Page(): ReactElement {
     return <App />;
