f59c9ab9fbf1ba9b4eff6036c55c68cea6cf8e64
Chafik H'nini <chafik.hnini@gmail.com>
2025-04-07 22:15:41 +0200

    fix: prevent components from rendering twice in dev

--- examples/supertrader/src/products/SuperTrader/server-nextjs/next.config.js
+++ examples/supertrader/src/products/SuperTrader/server-nextjs/next.config.js
@@ -7,18 +7,22 @@ export default {
     poweredByHeader: false,
 
     /**
+     * Prevent components from rendering twice in dev mode
+     * @see https://stackoverflow.com/questions/71847778/why-is-my-nextjs-component-rendering-twice
+     */
+    reactStrictMode: false,
+
+    /**
+     * Prevent packaging dependencies creating errors like "Module not found: Can't resolve 'oracledb'"
      * @see https://github.com/vercel/next.js/issues/53944
      * @see https://nextjs.org/docs/app/api-reference/config/next-config-js/serverExternalPackages
-     *
-     * > Dependencies used inside Server Components and Route Handlers will automatically be bundled by Next.js.
-     *
-     * If we don't include it, we get a "Module not found: Can't resolve 'oracledb'" error.
      */
     serverExternalPackages: ['knex'],
 
     typescript: {
         tsconfigPath: '../../../../tsconfig.json',
     },
+
     webpack: (config) => {
         config.resolve.extensionAlias = {
             '.js': (config.resolve.extensionAlias ?? []).concat([
