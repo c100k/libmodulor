bec416d9151e4fdb519d7a5615fbe6ab7df0237d
Chafik H'nini <chafik.hnini@gmail.com>
2025-05-27 15:46:05 +0200

    chore: switch to pnpm

--- examples/supertrader/build.sh
+++ examples/supertrader/build.sh
@@ -1,12 +1,12 @@
 # build all targets
-yarn tsc --project tsconfig.build.json
+pnpm tsc --project tsconfig.build.json
 
 # server specific
 cp .env dist/products/SuperTrader/server-node-express/.env
 cp .env dist/products/SuperTrader/server-node-hono/.env
 
 # web specific
-yarn vite -c src/products/SuperTrader/web/vite.config.ts build
+pnpm vite -c src/products/SuperTrader/web/vite.config.ts build
 
 # rn specific
 rm -Rf dist/products/SuperTrader/rn # let metro/babel transpiles the code
