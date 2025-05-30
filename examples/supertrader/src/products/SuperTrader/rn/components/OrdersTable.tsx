7440eda54b894ab57a607538b39871974e7cc56b
Chafik H'nini <chafik.hnini@gmail.com>
2025-05-30 17:22:10 +0200

    docs: add style to rn

--- examples/supertrader/src/products/SuperTrader/rn/components/OrdersTable.tsx
+++ examples/supertrader/src/products/SuperTrader/rn/components/OrdersTable.tsx
@@ -12,7 +12,7 @@ import {
     UCOutputFieldValue,
 } from 'libmodulor/react-native-pure';
 import React, { type ReactElement } from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 import {
     CancelOrderUCD,
@@ -47,43 +47,84 @@ export default function OrdersTable({
     }
 
     return (
-        <View>
-            <View style={{ gap: 8 }}>
+        <View style={styles.container}>
+            <View style={styles.itemsContainer}>
                 {items.map((i, idx) => (
-                    <View key={i.id} style={{ gap: 4 }}>
-                        <Text>{idx + 1}</Text>
-                        {fields.map((f) => (
-                            <View key={f.key}>
-                                <Text style={{ fontWeight: 'bold' }}>
-                                    {wordingManager.ucof(f.key).label}
-                                </Text>
-                                <Text>
-                                    <UCOutputFieldValue
-                                        f={f}
-                                        value={i[f.key]}
-                                    />
-                                </Text>
-                            </View>
-                        ))}
-                        <UCPanel
-                            onDone={async (ucor) => update0(ucor)}
-                            onError={onError}
-                            renderAutoExecLoader={UCAutoExecLoader}
-                            renderExecTouchable={UCExecTouchable}
-                            renderForm={UCForm}
-                            uc={new UC(Manifest, CancelOrderUCD, null).fill({
-                                id: i.id,
-                            })}
-                        />
+                    <View key={i.id} style={styles.item}>
+                        <View style={styles.itemContent}>
+                            <Text>{idx + 1}</Text>
+
+                            {fields.map((f) => (
+                                <View key={f.key}>
+                                    <Text style={styles.label}>
+                                        {wordingManager.ucof(f.key).label}
+                                    </Text>
+                                    <Text>
+                                        <UCOutputFieldValue
+                                            f={f}
+                                            value={i[f.key]}
+                                        />
+                                    </Text>
+                                </View>
+                            ))}
+                        </View>
+
+                        <View style={styles.itemActions}>
+                            <UCPanel
+                                onDone={async (ucor) => update0(ucor)}
+                                onError={onError}
+                                renderAutoExecLoader={UCAutoExecLoader}
+                                renderExecTouchable={UCExecTouchable}
+                                renderForm={UCForm}
+                                uc={new UC(Manifest, CancelOrderUCD, null).fill(
+                                    {
+                                        id: i.id,
+                                    },
+                                )}
+                            />
+                        </View>
                     </View>
                 ))}
             </View>
-            <View>
-                <View style={{ flexDirection: 'row', gap: 16 }}>
-                    <Text>{i18nManager.t('total')}</Text>
-                    <Text>{total}</Text>
-                </View>
+
+            <View style={styles.total}>
+                <Text style={styles.totalText}>{i18nManager.t('total')}</Text>
+                <Text style={styles.totalText}>{total}</Text>
             </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        gap: 16,
+    },
+    item: {
+        alignItems: 'center',
+        flexDirection: 'row',
+        gap: 4,
+        justifyContent: 'space-between',
+    },
+    itemActions: {
+        width: '30%',
+    },
+    itemsContainer: {
+        gap: 8,
+    },
+    itemContent: {
+        width: '70%',
+    },
+    label: {
+        fontWeight: 'bold',
+    },
+    total: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        gap: 16,
+    },
+    totalText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+});
