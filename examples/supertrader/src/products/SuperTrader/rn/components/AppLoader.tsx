88e8c495a2a0cf259acb483d65d18450949dbb61
Chafik H'nini <chafik.hnini@gmail.com>
2025-05-30 18:52:54 +0200

    refactor: extract components

---
+++ examples/supertrader/src/products/SuperTrader/rn/components/AppLoader.tsx
@@ -0,0 +1,17 @@
+import React, { type ReactElement } from 'react';
+import { ActivityIndicator, StyleSheet, View } from 'react-native';
+
+export default function AppLoader(): ReactElement {
+    return (
+        <View style={styles.container}>
+            <ActivityIndicator size="large" />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        height: 200,
+    },
+});
