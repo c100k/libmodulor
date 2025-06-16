24434be369c039ff5028e8f38e8a1b0fae1dcab1
Chafik H'nini <chafik.hnini@gmail.com>
2025-06-16 15:33:21 +0200

    feat: introduce server-cloudflare-worker target

--- examples/supertrader/build.sh
+++ examples/supertrader/build.sh
@@ -15,12 +15,16 @@ pnpm tsc --project tsconfig.build.json
 # noop
 
 # rn
-rm -Rf dist/$productPath/rn # let metro/babel transpile the code
+rm -Rf dist/$productPath/rn # let metro/babel handle it
 cp -R src/$productPath/rn dist/$productPath/
 echo '{"name":"rn","type":"module"}' > dist/$productPath/rn/package.json # required by expo
 
+# server-cloudflare-worker
+rm -Rf dist-examples/examples/$productPath/server-cloudflare-worker # let wrangler handle it
+cp -R src/$productPath/server-cloudflare-worker dist/$productPath/
+
 # server-nextjs
-rm -Rf dist/$productPath/server-nextjs # let next.js transpile the code
+rm -Rf dist/$productPath/server-nextjs # let next.js handle it
 cp -R src/$productPath/server-nextjs dist/$productPath/
 
 # server-node-express
