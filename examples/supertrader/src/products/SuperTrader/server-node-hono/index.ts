a373f92c2ff791af17460c8204f110a6db53b9af
Chafik H'nini <chafik.hnini@gmail.com>
2025-06-18 21:53:23 +0200

    refactor: rely on i18nManager automatically initialized

--- examples/supertrader/src/products/SuperTrader/server-node-hono/index.ts
+++ examples/supertrader/src/products/SuperTrader/server-node-hono/index.ts
@@ -1,13 +1,6 @@
-import {
-    APPS_ROOT_DIR_NAME,
-    type FSManager,
-    type I18nManager,
-    ServerBooter,
-} from 'libmodulor';
+import { APPS_ROOT_DIR_NAME, type FSManager, ServerBooter } from 'libmodulor';
 
 import container from './container.js';
-
-await container.get<I18nManager>('I18nManager').init();
 
 await container.get(ServerBooter).exec({
     appsRootPath: container
