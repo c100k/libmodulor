e6e72c7d26995013887334e9e5e74f95d337bb9b
Chafik H'nini <chafik.hnini@gmail.com>
2025-07-20 23:46:46 +0200

    docs(examples): adjust UCs descriptions

--- examples/standalone/src/apps/Toolbox/src/i18n.ts
+++ examples/standalone/src/apps/Toolbox/src/i18n.ts
@@ -2,7 +2,7 @@ import type { AppI18n } from 'libmodulor';
 
 export const I18n: AppI18n = {
     en: {
-        uc_ExportAsana_desc: 'Export your Asana tasks and projects',
+        uc_ExportAsana_desc: 'Export your Asana tasks and projects.',
         uc_ExportAsana_i_submit_idle: 'Export',
         uc_ExportAsana_i_submit_submitting: 'Exporting',
         uc_GenerateMiscData_desc:
@@ -11,10 +11,12 @@ export const I18n: AppI18n = {
         uc_GenerateMiscData_i_submit_submitting: 'Generating',
         uc_GenerateMiscData_op_0_label: 'Misc',
         uc_GenerateMiscData_op_1_label: 'UUIDs',
+        uc_GeocodeAddress_desc:
+            'Get the geolocation and Google Maps link for a French address (API adresse).',
         uc_GeocodeAddress_i_submit_idle: 'Geocode',
         uc_GeocodeAddress_i_submit_submitting: 'Geocoding',
         uc_PromptLLM_desc:
-            'Ask something about sports to an LLM (Mistral) and get a response back (BYOT (Bring Your Own Token))',
+            'Ask something about sports to an LLM (Mistral) and get a response back (BYOT (Bring Your Own Token)).',
         uc_PromptLLM_i_submit_idle: 'Send',
         uc_PromptLLM_i_submit_submitting: 'Sending',
         ucif_uuidCount_label: 'UUID count',
