2ad0fe789b631dfe2853b11df143d4fba9fee2ed
Chafik H'nini <chafik.hnini@gmail.com>
2025-07-27 22:42:50 +0200

    chore: plug app i18n to product i18n

--- examples/standalone/src/products/Main/i18n.ts
+++ examples/standalone/src/products/Main/i18n.ts
@@ -2,11 +2,13 @@ import type { ProductI18n } from 'libmodulor';
 import { I18nEN } from 'libmodulor/locales/en';
 
 import { I18n as MyRunningMapI18n } from '../../apps/MyRunningMap/index.js';
+import { I18n as MyTripI18n } from '../../apps/MyTrip/index.js';
 import { I18n as ToolboxI18n } from '../../apps/Toolbox/index.js';
 
 export const I18n: ProductI18n = {
     en: {
         ...I18nEN,
+        ...MyTripI18n.en,
         ...MyRunningMapI18n.en,
         ...ToolboxI18n.en,
     },
