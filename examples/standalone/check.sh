2058ffb24c0dc92968ed9174961674d67940cb7d
Chafik H'nini <chafik.hnini@gmail.com>
2025-09-18 22:30:07 +0200

    chore: enhance standalone check

--- examples/standalone/check.sh
+++ examples/standalone/check.sh
@@ -1,6 +1,10 @@
 set -e
 
-pnpm test
-pnpm libmodulor GenerateAppsTests
-pnpm libmodulor TestApp --appName MyRunningMap
-pnpm libmodulor TestApp --appName Toolbox
+time (
+    pnpm test
+    pnpm libmodulor GenerateAppsTests
+    pnpm libmodulor TestApp --appName MyRunningMap &
+    pnpm libmodulor TestApp --appName MyTrip &
+    pnpm libmodulor TestApp --appName Toolbox &
+    wait
+)
