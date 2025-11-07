a1a42420c09531997c2ece58a080c5cb67a8f1c3
Chafik H'nini <chafik.hnini@gmail.com>
2025-11-07 21:20:59 +0100

    feat(target client): set server_cookies_name_auth default value

--- dist/esm/target/lib/client/consts.js
+++ dist/esm/target/lib/client/consts.js
@@ -2,6 +2,7 @@
  * @see TARGET_DEFAULT_SERVER_MANAGER_SETTINGS
  */
 export const TARGET_DEFAULT_SERVER_CLIENT_MANAGER_SETTINGS = {
+    server_cookies_name_auth: 'auth',
     server_public_api_key: 'PublicApiKeyToBeChangedWhenDeploying',
     server_public_api_key_header_name: 'X-API-Key',
     server_public_url: 'http://localhost:7443',
