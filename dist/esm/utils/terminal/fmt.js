407500e6b9119728b4fd0ce06e890e37947697bb
Chafik H'nini <chafik.hnini@gmail.com>
2025-11-22 13:03:16 +0100

    fix(utils terminal): keep original array sorting in fmtPadEndFor

--- dist/esm/utils/terminal/fmt.js
+++ dist/esm/utils/terminal/fmt.js
@@ -5,7 +5,7 @@ export function fmtCommand(cmd, details, padEnd) {
     return `  ${cmd.padEnd(padEnd)}${details ?? ''}`;
 }
 export function fmtPadEndFor(s) {
-    const longest = s.sort((a, b) => a.length - b.length).reverse()[0];
+    const longest = [...s].sort((a, b) => a.length - b.length).reverse()[0];
     return (longest?.length ?? 1) + 10;
 }
 export function fmtSection(s) {
