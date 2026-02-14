168893f91608d5f378d39a0aaea1dba21994a5f1
Chafik H'nini <chafik.hnini@gmail.com>
2026-02-14 10:39:49 +0100

    chore(deps): bump minor/patch dependencies

--- dist/esm/apps/Helper/src/lib/layers/project.js
+++ dist/esm/apps/Helper/src/lib/layers/project.js
@@ -88,7 +88,7 @@ export const PACKAGE_JSON = (name) => `{
     },
     "devDependencies": {
         "@biomejs/biome": "^2.3.14",
-        "@types/node": "^25.2.0",
+        "@types/node": "^25.2.3",
         "@vitest/coverage-v8": "^3.2.4",
         "buffer": "^6.0.3",
         "cookie-parser": "^1.4.7",
