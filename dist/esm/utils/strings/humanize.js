2e714989787f83e7c9a27c3fc610aae0fe26ea0f
Chafik H'nini <chafik.hnini@gmail.com>
2026-05-31 22:32:44 +0200

    feat(utils): humanize snake case strings

--- dist/esm/utils/strings/humanize.js
+++ dist/esm/utils/strings/humanize.js
@@ -1,5 +1,8 @@
 export function humanize(value) {
-    let res = value.replace(/([a-z])([A-Z])/g, '$1 $2').trim();
+    let res = value
+        .replace(/([a-z])([A-Z])/g, '$1 $2')
+        .replace(/([a-z])_([a-z])/g, '$1 $2')
+        .trim();
     res = res.charAt(0).toUpperCase() + res.slice(1);
     return res;
 }
